915272f66e172a88d731e894f23c5c078ab5d087
mihai-leo <139263123+mihai-leo@users.noreply.github.com>
2024-03-12 19:13:12 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.project1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.project1 to javafx.fxml;
-    exports com.example.project1;
-}
